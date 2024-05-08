3b52d15353faf62f71fac2a49d2ef377bba4bb96
MoamenRady <moamen.elmoharby.51@gamil.com>
2024-05-08 08:17:04 +0300

    final

--- mano_basic_computer_using_verilog_by_limit.sim/sim_1/behav/xsim/xsim.dir/HEAD_TB_behav/webtalk/xsim_webtalk.tcl
+++ mano_basic_computer_using_verilog_by_limit.sim/sim_1/behav/xsim/xsim.dir/HEAD_TB_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
 webtalk_init -webtalk_dir D:/mano_basic_computer_using_verilog_by_limit/mano_basic_computer_using_verilog_by_limit.sim/sim_1/behav/xsim/xsim.dir/HEAD_TB_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Wed May  8 08:07:01 2024" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Wed May  8 08:16:22 2024" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2018.2 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2258646" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "ebcae50f-2652-4c52-b51d-d14543aa9c58" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "7ac72585ba2044c69ecd0dc1ecdffe3c" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "46" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "48" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i7-4810MQ CPU @ 2.80GHz" -context "user_environment"
@@ -26,7 +26,7 @@ webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "1" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.03_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "6752_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 768900924 -regid "" -xml D:/mano_basic_computer_using_verilog_by_limit/mano_basic_computer_using_verilog_by_limit.sim/sim_1/behav/xsim/xsim.dir/HEAD_TB_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/mano_basic_computer_using_verilog_by_limit/mano_basic_computer_using_verilog_by_limit.sim/sim_1/behav/xsim/xsim.dir/HEAD_TB_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/mano_basic_computer_using_verilog_by_limit/mano_basic_computer_using_verilog_by_limit.sim/sim_1/behav/xsim/xsim.dir/HEAD_TB_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.00_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "6688_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 1129538301 -regid "" -xml D:/mano_basic_computer_using_verilog_by_limit/mano_basic_computer_using_verilog_by_limit.sim/sim_1/behav/xsim/xsim.dir/HEAD_TB_behav/webtalk/usage_statistics_ext_xsim.xml -html D:/mano_basic_computer_using_verilog_by_limit/mano_basic_computer_using_verilog_by_limit.sim/sim_1/behav/xsim/xsim.dir/HEAD_TB_behav/webtalk/usage_statistics_ext_xsim.html -wdm D:/mano_basic_computer_using_verilog_by_limit/mano_basic_computer_using_verilog_by_limit.sim/sim_1/behav/xsim/xsim.dir/HEAD_TB_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
--- mano_basic_computer_using_verilog_by_limit.srcs/sources_1/new/ASIGNMENTAT.v
+++ mano_basic_computer_using_verilog_by_limit.srcs/sources_1/new/ASIGNMENTAT.v
@@ -21,26 +21,23 @@
 
 
 module ASIGNMENTAT
- ( input CLK,
-  input [7:0] T,
-   input cout,
-    input [15:0] AC,
-     input [15:0] IR,
-                     output CLRAR,
-                     output INRAR,
-                     output LoadAR,
-                     output CLRPC,
-                     output INRPC,
-                     output LoadPC,
+ (                   
+                     
                      output CLRAC,
-                     output INRAC,
-                     output LoadAC,
+                     output INCAC,
+                     output LDAC,
                      output CLRDR,
-                     output INRDR,
-                     output LoadDR,
+                     output INCDR,
+                     output LDDR,
+                     output CLRAR,
+                     output INCAR,
+                     output LDAR,
+                     output CLRPC,
+                     output INCPC,
+                     output LDPC,
                      output CLRIR,
-                     output INRIR,
-                     output LoadIR,
+                     output INCIR,
+                     output LDIR,
                      output WRITE,
                      output READ,
                      output CMA, 
@@ -48,7 +45,7 @@ module ASIGNMENTAT
                      output ADD,
                      output AND,
                      output CLRSC,
-                     output INRSC,
+                     output INCSC,
                      output E,
                      output [7:0] Xs,
                      output I,
@@ -56,7 +53,12 @@ module ASIGNMENTAT
                      output CIR,
                      output CIL, 
                      output INP,
-                     input [15:0] DR );
+                     input [15:0] DR,
+                     input CLK,
+                     input [7:0] T,
+                     input cout,
+                     input [15:0] AC,
+                     input [15:0] IR );
                      
 
 
@@ -73,11 +75,11 @@ module ASIGNMENTAT
 	
 	DECODER	dec(OPCODE , DECODER_OUTPUT[7:0]); 
     assign D = DECODER_OUTPUT;
-	AR_CONTROL ar(LoadAR, CLRAR, INRAR, T, D, I);
-	PC_CONTROL pc(CLRPC, INRPC, LoadPC,I, E, T, D, B,DR , AC);  	
-	DR_CONTROL dr(LoadDR, INRDR, T, D);  	
-	AC_CONTROL ac(AND, CMA, LDA, ADD, LoadAC, INRAC, CLRAC,CIR,INP,CIL, T, D, B, I); 
-	IR_CONTROL ir(D, T, CLRIR, INRIR, LoadIR); 
+	AR_CONTROL ar(LDAR, CLRAR, INCAR, T, D, I);
+	PC_CONTROL pc(CLRPC, INCPC, LDPC,I, E, T, D, B,DR , AC);  	
+	DR_CONTROL dr(LDDR, INCDR, T, D);  	
+	AC_CONTROL ac(AND, CMA, LDA, ADD, LDAC, INCAC, CLRAC,CIR,INP,CIL, T, D, B, I); 
+	IR_CONTROL ir(D, T, CLRIR, INCIR, LDIR); 
 	RAM_CONTROL ram(WRITE, READ, T, D, I);	
 	SEQ_COUNTER_CLR sc(CLRSC, INRSC, T, D, I);	
 	COMMON_BUS_CONTROL bus(Xs, D, T, I);   
